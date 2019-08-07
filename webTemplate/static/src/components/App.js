cff851ec2891332e0738cc1051c490a76d21d3d9
z33p <46050958+z33p@users.noreply.github.com>
2019-08-07 19:29:48 -0300

    Create App.js

---
+++ webTemplate/static/src/components/App.js
@@ -0,0 +1,18 @@
+
+import React, { Component } from 'react';
+import Main from './Main';
+
+
+class App extends Component {
+    state = {}
+
+    render() {
+        return (
+          
+            <Main />
+          
+         );
+    }
+}
+
+export default App;
